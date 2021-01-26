17ae1df2b555427a073c7627ad87b881638d1ca5
Edgar Lopez <edgarlv_1998@hotmail.com>
2021-01-26 15:54:44 -0600

    github-finder

---
+++ src/components/users/UsersItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const UsersItem = ({user: {login, avatar_url, html_url}}) => {
+
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} className="round-img" style={{width: '60px'}} alt=""/>
+            <h3>{login}</h3>
+            <div>
+                <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
+            </div>
+        </div>
+    )
+}
+
+UsersItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
+export default UsersItem
